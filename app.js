5d50c827381b44762389f8aff0d610883adc2ce0
Camila <camilalrocca@gmail.com>
2022-11-04 22:33:40 -0300

    Cambio websocket

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import StompJs from 'stompjs';
 import SockJS from 'sockjs-client';
 
 var stompClient = null;
-var socket = new SockJS('http://core.deliver.ar/websocket');
+var socket = new SockJS('http://core.deliver.ar/cliente'); //cambiar cliente por el modulo que corresponda
     
 stompClient = StompJs.over(socket);
 stompClient.connect({}, function (frame) {
     console.log('Connected: ' + frame);
-    stompClient.subscribe('/topic/user', function (user) {
+    stompClient.subscribe('/topic/cliente', function (user) { //cambiar cliente por el modulo que corresponda
             processMessage(JSON.parse(user.body));
         });
     stompClient.send("/app/cliente"); //cambiar cliente por el modulo que corresponda
